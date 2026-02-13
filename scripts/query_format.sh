fa1a74b1c179b1d415d9815039eac2ccf9461a7c
Joseph Doundoulakis <iosif.doundoulakis@outlook.com>
2026-02-13 06:45:49 +0100

    chore: cleaned query-building script

--- scripts/query_format.sh
+++ scripts/query_format.sh
@@ -4,14 +4,14 @@ Q+="+and+ContentDate/Start+lt+$3T00:00:00.000Z"
 Q+="+and+Attributes/OData.CSC.StringAttribute/any("
 Q+="att:att/Name+eq+'productType'"
 Q+="+and+att/OData.CSC.StringAttribute/Value+eq+'$1')"
-        
+
 if [[ $1 == "S2MSI1C" ]]
 then
 	Q+="+and+Attributes/OData.CSC.DoubleAttribute/any("
 	Q+="att:att/Name+eq+'cloudCover'"
-				# BUGFIX DoubleAttribute were changed in latest API
-				# and are now expecting float format. E.g. Previously
-				# expected '15.00' becomes simply 15.00.
+	# BUGFIX DoubleAttribute were changed in latest API
+	# and are now expecting float format. E.g. Previously
+	# expected '15.00' becomes simply 15.00.
 	Q+="+and+att/OData.CSC.DoubleAttribute/Value+lt+15.00)"
 fi
 
